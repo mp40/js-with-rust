4470d4c51e289591265b895d5b8d2d7d0dd368b2
mp40 <philps.matthew@gmail.com>
2023-04-22 15:50:39 +0200

    import rs find nth method

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+import ffi from "ffi-napi";
 import { findNth } from "./js/fib.js";
 
+const lib = ffi.Library("./rs/fib/target/release/libfind_nth", {
+  find_nth: ["int", ["int"]],
+});
+
 function run() {
-  console.time("getNth JS");
-  const jsResult = findNth(100);
+  console.time("getNth 20 JS");
+  const jsResult20 = findNth(20);
   console.timeEnd("getNth JS");
-  console.log("JS Result > ", jsResult);
+  console.log("JS Get 20 Result > ", jsResult20);
+
+  console.time("getNth 20 RS");
+  const rsResult20 = lib.find_nth(20);
+  console.timeEnd("getNth 20 RS");
+  console.log("RS Get 20 Result > ", rsResult20);
+
+  console.time("getNth 100 JS");
+  const jsResult100 = findNth(100);
+  console.timeEnd("getNth JS");
+  console.log("JS Get 100 Result > ", jsResult100);
+
+  console.time("getNth 100 RS");
+  const rsResult100 = lib.find_nth(100);
+  console.timeEnd("getNth RS");
+  console.log("RS Get 100 Result > ", rsResult100);
 }
 
 run();
---
+++ rs/fib/src/lib.rs
@@ -0,0 +1,59 @@
+#[no_mangle]
+pub extern fn find_nth(n:i64) -> i64 {
+    if n == 0 {
+        return 0;
+    }
+
+    if n < 3 {
+        return 1;
+    }
+
+    let mut prev2 = 1;
+    let mut prev1 = 1;
+    let mut index = 3;
+
+    while index < n {
+        let current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+        index += 1;
+    }
+
+    return prev1 + prev2;
+}
+
+#[test]
+fn test_zero_position() {
+    let result = find_nth(0);
+    assert_eq!(result, 0);
+}
+
+#[test]
+fn test_first_position() {
+    let result = find_nth(1);
+    assert_eq!(result, 1);
+}
+
+#[test]
+fn test_second_position() {
+    let result = find_nth(2);
+    assert_eq!(result, 1);
+}
+
+#[test]
+fn test_third_position() {
+    let result = find_nth(3);
+    assert_eq!(result, 2);
+}
+
+#[test]
+fn test_fourth_position() {
+    let result = find_nth(4);
+    assert_eq!(result, 3);
+}
+
+#[test]
+fn test_twentieth_position() {
+    let result = find_nth(20);
+    assert_eq!(result, 6765);
+}
