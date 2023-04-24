e3bd243a8e98cceae4ed96e62c41622370f0ac91
Matthew Philps <30450957+mp40@users.noreply.github.com>
2023-04-24 08:54:20 +0200

    update readme and comments

--- README.md
+++ README.md
@@ -38,12 +38,12 @@ Works fine with smaller numbers, but not larger.
 Example compare result:
 
 ```
-getNth 20 JS: 0.112ms
+getNth 20 JS: 0.098ms
 JS Get 20 Result >  6765
-getNth 20 RS: 0.111ms
+getNth 20 RS: 0.106ms
 RS Get 20 Result >  6765
-getNth 100 JS: 0.007ms
-JS Get 100 Result >  354224848179262000000
-getNth 100 RS: 0.041ms
-RS Get 100 Result >  -980107325
+getNth 78 JS: 0.005ms
+JS Get 78 Result >  8944394323791464
+getNth 78 RS: 0.033ms
+RS Get 78 Result >  375819880
 ```
--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const lib = ffi.Library("./rs/fib/target/release/libfind_nth", {
   find_nth: ["int", ["int"]],
 });
 
+/*
+  78th position is largest that will return a value <= Number.MAX_SAFE_INTEGER
+*/
+
 function run() {
   console.time("getNth 20 JS");
   const jsResult20 = findNth(20);
@@ -19,12 +23,12 @@ function run() {
   console.time("getNth 78 JS");
   const jsResult78 = findNth(78);
   console.timeEnd("getNth 78 JS");
-  console.log("JS Get 100 Result > ", jsResult78);
+  console.log("JS Get 78 Result > ", jsResult78);
 
   console.time("getNth 78 RS");
   const rsResult78 = lib.find_nth(78);
   console.timeEnd("getNth 78 RS");
-  console.log("RS Get 100 Result > ", rsResult78);
+  console.log("RS Get 78 Result > ", rsResult78);
 }
 
 run();
